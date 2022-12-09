718e97b46d53cb6a01d599fb8b15f38d2c2d8d71
Jordan-AA <119890764+Jordan-AA@users.noreply.github.com>
2022-12-09 14:16:35 +0100

    Add files via upload

---
+++ Project Jordan.py
@@ -0,0 +1,27 @@
+# Welcome to the Decision Helper by Anna Jordan!
+
+import random
+
+entered_question = "abc"
+
+Answers = ["For sure!", "Absolutely yes", "Most likely", "Yes, definitely", 
+"No, no way", "Absolutely not", "Don't even think about it" , "Just no", 
+"I have absolutely no idea", "As far as I see, yes", "Totally no"]
+
+f = open("Decision helper.txt", "a+")
+i = 1
+while entered_question != "":
+    print ("Enter your question. Press ENTER to quit")
+    entered_question = input()
+    print (entered_question)
+    f.write (entered_question + "\n")
+   
+    showed_answer = Answers [random.randint(0,10)]
+    print (showed_answer)
+    f.write(showed_answer + "\n")
+    
+    if i==3:
+        print("Stop asking questions! Go do some work. I saved your answers for later. Find me as ""Decision helper! ")
+        break
+    i+= 1
+f.close()
